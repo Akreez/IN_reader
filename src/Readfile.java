7805f671e9fdb63153d25aab0349b00a778273ee
13asTerem <you@example.com>
2025-09-30 09:05:40 +0200

    Kész

--- src/Readfile.java
+++ src/Readfile.java
@@ -1,3 +1,29 @@
-public class Readfile {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Readfile implements Readable{
+
+    @Override
+    public void readContent(){
+        try {
+            tryReadContent();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public void tryReadContent() throws FileNotFoundException{
+        File file = new File("adat.txt");
+        Scanner sc = new Scanner(file, "UTF-8");
+        StringBuffer sb = new StringBuffer();
+        while (sc.hasNextLine()) {
+            sb.append(sc.nextLine());
+            sb.append('\n');
+        }
+        String content = sb.toString();
+        System.out.println("Tartalom:");
+        System.out.println(content);
+        sc.close();
+    }
+
 }
